5fa8c32ea80fd18cd704b26d22bcf5c40edd842c
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2023-02-16 13:46:07 +0800

    Create class MainWindow

--- Final Project.py
+++ Final Project.py
@@ -2,3 +2,11 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
